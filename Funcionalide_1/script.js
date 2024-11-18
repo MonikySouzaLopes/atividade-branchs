0b13c6f74cf85df5454952da4644231d7d39724c
Moniky <monikysouzalopes@gmail.com>
2024-11-18 20:30:13 -0300

    Implementando a funcionalidade 1

---
+++ Funcionalide_1/script.js
@@ -0,0 +1,75 @@
+let calcular = function(){
+    
+    let numero =  document.getElementById("Numero").value
+    numero = Number(numero)
+  
+    
+  let resultado = numero * 0 
+   resultado = Number(resultado)
+  
+   let resultado1 = numero * 1 
+   resultado1 = Number(resultado1)
+   
+   let resultado2 = numero * 2 
+   resultado2 = Number(resultado2)
+   
+   let resultado3 = numero * 3 
+   resultado3 = Number(resultado3)
+
+   let resultado4 = numero * 4 
+   resultado4 = Number(resultado4)
+
+   let resultado5 = numero * 5 
+   resultado5 = Number(resultado5)
+
+   let resultado6 = numero * 6 
+   resultado6 = Number(resultado6)
+
+   let resultado7 = numero * 7 
+   resultado7 = Number(resultado7)
+
+   let resultado8 = numero * 8 
+   resultado8 = Number(resultado8)
+
+   let resultado9 = numero * 9 
+   resultado9 = Number(resultado9)
+
+   let resultado10 = numero * 10 
+   resultado10 = Number(resultado10)
+
+
+    let saídaResultado = document.getElementById("Resultado")
+    saídaResultado.innerHTML = resultado
+
+    let saídaResultado1 = document.getElementById("Resultado1")
+    saídaResultado1.innerHTML = resultado1
+
+    let saídaResultado2 = document.getElementById("Resultado2")
+    saídaResultado2.innerHTML = resultado2
+
+    let saídaResultado3 = document.getElementById("Resultado3")
+    saídaResultado3.innerHTML = resultado3
+
+    let saídaResultado4 = document.getElementById("Resultado4")
+    saídaResultado4.innerHTML = resultado4
+
+    let saídaResultado5 = document.getElementById("Resultado5")
+    saídaResultado5.innerHTML = resultado5
+
+    let saídaResultado6 = document.getElementById("Resultado6")
+    saídaResultado6.innerHTML = resultado6
+
+    let saídaResultado7 = document.getElementById("Resultado7")
+    saídaResultado7.innerHTML = resultado7
+
+    let saídaResultado8  = document.getElementById("Resultado8")
+    saídaResultado8.innerHTML = resultado8
+
+    let saídaResultado9 = document.getElementById("Resultado9")
+    saídaResultado9.innerHTML = resultado9
+
+    let saídaResultado10 = document.getElementById("Resultado10")
+    saídaResultado10.innerHTML = resultado10
+
+    
+   }
